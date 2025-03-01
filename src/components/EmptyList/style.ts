b52ed81f5be142b89b971b9bb28558e48f80160f
GabrielLaminas <93532694+GabrielLaminas@users.noreply.github.com>
2025-03-01 11:32:34 -0300

    create empty list component

---
+++ src/components/EmptyList/style.ts
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const style = StyleSheet.create({
+  viewContainer: {
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 48,
+    borderWidth: 1,
+    borderTopColor: '#333333',
+  },
+  imageContainer: {
+    marginBottom: 16,
+    alignItems: 'center'
+  },
+  textEmpty: {
+    color: "#808080",
+    fontSize: 14,
+    fontFamily: 'inter-regular',
+    fontWeight: '400',
+    lineHeight: 21,
+    textAlign: 'center'
+  },
+  textEmptyBold: {
+    fontFamily: 'inter-bold',
+    fontWeight: 'bold',
+  }
+})
+
+export default style;
